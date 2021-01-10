b407868a3b627bae33637511fddd5b3cf2dd135f
Ewan Gilligan <eg207@st-andrews.ac.uk>
2021-01-10 12:07:37 +0000

    Add structs for data

---
+++ plotting/src/bench_data.rs
@@ -0,0 +1,17 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BenchGroup {
+    pub group_name : String,
+    pub ids : Vec<BenchID>
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BenchID {
+    pub id : String,
+    pub entries : Vec<BenchEntry>
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BenchEntry {
+    pub val : u64,
+    pub times : Vec<u64>
+}
